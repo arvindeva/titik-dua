18d680345833a66b34d8b74c2e26592330d12c22
arvindeva <arvindeva@gmail.com>
2024-03-02 01:03:20 +0100

    begin with stay, goodnight

--- components/home/stay.tsx
+++ components/home/stay.tsx
@@ -1,7 +1,12 @@
+import { kobe } from '@/app/fonts/fonts';
+import { cn } from '@/lib/utils';
+
 export default function Stay() {
   return (
-    <section className="bg-white w-full">
-      <h1>stay</h1>
+    <section className="bg-white w-full py-5 px-4">
+      <h1 className={cn('text-[40px] font-bold tracking-wide', kobe.className)}>
+        stay
+      </h1>
       <div className="carousel" />
     </section>
   );
