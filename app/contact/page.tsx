fb9b6f64abc33ab7e13d0d3d3655d1816d50ba37
arvindeva <arvindeva@gmail.com>
2024-03-06 18:39:15 +0100

    starting contact page, added form,label,textarea components

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,10 +3,20 @@ import { kobe } from "../fonts/fonts";
 
 export default function ContactPage() {
   return (
-    <main className="bg-td-beige h-[calc(100vh-80px)] px-4 pt-24 mx-auto text-center">
-      <h1 className={cn("font-black text-4xl", kobe.className)}>
-        under construction
-      </h1>
+    <main className="">
+      <div className="bg-td-beige py-10 px-4">
+        <h1 className={cn("font-bold text-[56px]", kobe.className)}>contact</h1>
+      </div>
+      <section className="px-4 py-10">
+        <h1 className={cn("font-bold text-[40px] mb-10", kobe.className)}>
+          get in touch with us
+        </h1>
+        <p className="text-sm leading-tight">
+          Your feedback, questions, and enquiries are important to us. Let us
+          know what you enjoyed, what we could do better, or anything else
+          you&apos;d like to share.
+        </p>
+      </section>
     </main>
   );
 }
